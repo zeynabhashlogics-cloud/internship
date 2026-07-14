004200350bffd39ec59211700aac6a8ee1fb5858
zeynab <zeynab.hashlogics@gmail.com>
2026-07-14 15:58:20 +0500

    PROFILE

---
+++ profile.js
@@ -0,0 +1,8 @@
+let name="zeynab";
+let age = 19;
+let skills ="computer science";
+let availibility=false;
+console.log(`Name = ${name}`);//string
+console.log("age = " + age); //numbers
+console.log("skills = " + skills); //string
+console.log("availibility = " + (availibility ? "available" : "not available ")); //boolean
