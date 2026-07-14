3c112f810afcac4e1603b51c027fb0888e02ae15
zeynab <zeynab.hashlogics@gmail.com>
2026-07-14 16:38:15 +0500

    calculator

--- calc2.js
+++ calc2.js
@@ -7,8 +7,10 @@ let add = a+b;
 let sub =a-b;
 let div=a/b;
 let mul =a*b;
+let prcnt =((a/b)* 100);
 
 console.log("addition = " + add);
 console.log("subtraction = " + sub);
 console.log("multiplication = " + mul);
 console.log("division = " + div);
+console.log("percentage = " + prcnt);
