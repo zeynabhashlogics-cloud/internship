6785a0b5f89aff63464f266a1801837216b532f8
zeynab <zeynab.hashlogics@gmail.com>
2026-07-14 16:14:13 +0500

    calc

---
+++ calculator.js
@@ -0,0 +1,12 @@
+let a =20;
+let b=30;
+
+let add = a+b;
+let sub =a-b;
+let div=a/b;
+let mul =a*b;
+
+console.log("addition = " + add);
+console.log("subtraction = " + sub);
+console.log("multiplication = " + mul);
+console.log("division = " + div);
